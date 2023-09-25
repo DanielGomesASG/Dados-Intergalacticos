91ddb4e6faf72c77389c75cb877532e54ba930f5
Daniel Gomes <danielgomesasg@gmail.com>
2023-09-25 00:11:59 -0300

    Otimizando a lógica

--- mainScript.js
+++ mainScript.js
@@ -3,66 +3,45 @@ const y = prompt("Muito bem!\n\nAgora insira um id entre 1 e 60, para encontrar 
 const z = prompt("Estamos quase lá!\n\nAgora insira um id entre 1 e 36, para encontrar os dados da nave que deseja.")
 alert("Analisando as informações interplanetárias da pessoa...\n\nAnalisando o mapa intergaláctico dos planetas...\n\nAvaliando as naves disponíveis no estoque...\n\nProcesso finalizado. Clique em OK e acesse os dados que conseguimos encontrar no console! :D")
 
-const loadPerson = (x) => {
-  const xhr1 = new XMLHttpRequest()
+const url = "https://swapi.dev/api/"
+function tratar(xhr, texto) {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const response = JSON.parse(xhr.responseText)
+    console.log(`Estes são os dados de ${texto}:`, response, "\n\n...")
+    return response
+  } else {
+    console.log(`Infelizmente os dados de ${texto} estão fora do ar no momento, sorry! :/\n\n...`)
+  }
+} 
 
-  xhr1.open("GET", "https://swapi.dev/api/people/" + x)
-  xhr1.send(null)
-  xhr1.onreadystatechange = () => {
-    if (x > 0 && x <= 82) {
-      if (xhr1.readyState === 4) {
-        if (xhr1.status === 200) {
-          const person = JSON.parse(xhr1.responseText)
-          console.log("Estes são os dados da pessoa:\n", person, "\n\n...")
-        } else {
-          console.log("Infelizmente os dados dessa pessoa estão fora do ar no momento, sorry! :/\n\n...")
-        }
-      }
-    } else {
-      console.log("Id de pessoa inválido! Dados perdidos no vazio do espaço! D:\n\n...")
-    }
+
+const loadPerson = (x) => {
+  const xhr = new XMLHttpRequest()
+
+  xhr.open("GET", `${url}people/${x}`)
+  xhr.send(null)
+  xhr.onreadystatechange = () => {
+    xhr.readyState === 4 ? tratar(xhr, "pessoa") : null
   }
 }
 
 const loadPlanet = (y) => {
-  const xhr2 = new XMLHttpRequest()
+  const xhr = new XMLHttpRequest()
 
-  xhr2.open("GET", "https://swapi.dev/api/planets/" + y)
-  xhr2.send(null)
-  xhr2.onreadystatechange = () => {
-    if (y > 0 && y <= 60) {
-      if (xhr2.readyState === 4) {
-        if (xhr2.status === 200) {
-          const planet = JSON.parse(xhr2.responseText)
-          console.log("Estes são os dados do planeta:\n", planet, "\n\n...")
-        } else {
-          console.log("Infelizmente os dados desse planeta estão fora do ar no momento, sorry! :/\n\n...")
-        }
-      }
-    } else {
-      console.log("Id de planeta inválido! Dados perdidos no vazio do espaço! D:\n\n...")
-    }
+  xhr.open("GET", `${url}planets/${y}`)
+  xhr.send(null)
+  xhr.onreadystatechange = () => {
+    xhr.readyState === 4 ? tratar(xhr, "planeta") : null
   }
 }
 
 const loadShip = (z) => {
-  const xhr3 = new XMLHttpRequest()
+  const xhr = new XMLHttpRequest()
 
-  xhr3.open("GET", "https://swapi.dev/api/starships/" + z)
-  xhr3.send(null)
-  xhr3.onreadystatechange = () => {
-    if (z > 0 && z <= 60) {
-      if (xhr3.readyState === 4) {
-        if (xhr3.status === 200) {
-          const ship = JSON.parse(xhr3.responseText)
-          console.log("Estes são os dados da nave:\n", ship, "\n\n...")
-        } else {
-          console.log("Infelizmente os dados dessa nave estão fora do ar no momento, sorry! :/\n\n...")
-        }
-      }
-    } else {
-      console.log("Id de planeta inválido! Dados perdidos no vazio do espaço! D:\n\n...")
-    }
+  xhr.open("GET", `${url}starships/${z}`)
+  xhr.send(null)
+  xhr.onreadystatechange = () => {
+    xhr.readyState === 4 ? tratar(xhr, "nave") : null
   }
 }
 
